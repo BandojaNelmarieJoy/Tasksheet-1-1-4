7e6a09588ec3edd998a864127478be3484715e40
BandojaNelmarieJoy <150577420+BandojaNelmarieJoy@users.noreply.github.com>
2023-12-03 17:14:23 +0800

    Add files via upload

---
+++ Tasksheet114.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasksheet1.pkg1.pkg4;
+
+/**
+ *
+ * @author NelJoy
+ */
+public class Tasksheet114 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int score = 91;
+        if(score>90 && score<=100)
+            System.out.println("A grade");
+        if(score>80 && score<=89)
+            System.out.println("B grade");
+        if(score>70 && score<=79)
+            System.out.println("C grade");
+        if(score>60 && score<=69)
+            System.out.println("D grade");
+        if(score>50 && score<=59)
+            System.out.println("F grade");
+    }
+}
